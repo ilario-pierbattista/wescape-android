e49cd3cdb0b63e567584ee52ce77ac5e6bcd535d
toioski <toioski@hotmail.it>
2016-02-14 19:05:08 +0100

    Completata transizione colori

--- wescape/src/main/java/com/dii/ids/application/main/navigation/NavigationActivity.java
+++ wescape/src/main/java/com/dii/ids/application/main/navigation/NavigationActivity.java
@@ -2,12 +2,12 @@ package com.dii.ids.application.main.navigation;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,10 +18,7 @@ import com.dii.ids.application.R;
 
 public class NavigationActivity extends AppCompatActivity {
 
-    private Toolbar mToolbar;
-    private TextView mToolbarTitle;
-    private View mRevealView;
-    private View mRevealBackgroundView;
+    private ViewHolder holder;
     private boolean emergency = false;
 
 
@@ -29,20 +26,20 @@ public class NavigationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+        holder = new ViewHolder(findViewById(android.R.id.content));
+
 
         // Set the Toolbar as the ActionBar
-        mToolbar = (Toolbar) findViewById(R.id.navigation_toolbar);
-        setSupportActionBar(mToolbar);
+        setSupportActionBar(holder.toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        mToolbarTitle = (TextView) mToolbar.findViewById(R.id.navigation_toolbar_textview_title);
-        mRevealView = findViewById(R.id.reveal);
-        mRevealBackgroundView = findViewById(R.id.revealBackground);
-
+        // Setup textview text
+        holder.destinationViewPlaceholder.setText(R.string.navigation_going_to);
+        holder.originViewText.setText(R.string.navigation_select_origin);
+        holder.destinationViewText.setText(R.string.navigation_select_destination);
 
         // Set the Fab button
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        holder.startFabButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -74,14 +71,21 @@ public class NavigationActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Update view for normal/emergency state
+     */
     private void toogleEmergency() {
         if (!emergency) {
             animateAppAndStatusBar(R.color.regularBlue, R.color.regularRed);
-            mToolbarTitle.setText(R.string.action_emergency);
+            holder.startFabButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.regularRed)));
+            holder.toolbarTitle.setText(R.string.action_emergency);
+            holder.destinationViewText.setText("La destinazione verrà impostata automaticamente");
             emergency = true;
         } else {
-            mToolbarTitle.setText(R.string.title_activity_navigation);
             animateAppAndStatusBar(R.color.regularRed, R.color.regularBlue);
+            holder.startFabButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.regularBlue)));
+            holder.toolbarTitle.setText(R.string.title_activity_navigation);
+            holder.destinationViewText.setText(R.string.navigation_select_destination);
             emergency = false;
         }
     }
@@ -94,22 +98,56 @@ public class NavigationActivity extends AppCompatActivity {
      */
     private void animateAppAndStatusBar(int fromColor, final int toColor) {
         Animator animator = ViewAnimationUtils.createCircularReveal(
-                mRevealView,
-                mToolbar.getWidth() / 2,
-                mToolbar.getHeight() / 2, 0,
-                mToolbar.getWidth() / 2);
+                holder.revealView,
+                holder.toolbar.getWidth() / 2,
+                holder.toolbar.getHeight() / 2, 0,
+                holder.toolbar.getWidth() / 2);
 
         animator.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationStart(Animator animation) {
-                mRevealView.setBackgroundColor(getResources().getColor(toColor));
+                holder.revealView.setBackgroundColor(getResources().getColor(toColor));
             }
         });
 
-        mRevealBackgroundView.setBackgroundColor(getResources().getColor(fromColor));
+        holder.revealBackgroundView.setBackgroundColor(getResources().getColor(fromColor));
         animator.setStartDelay(50);
         animator.setDuration(125);
         animator.start();
-        mRevealView.setVisibility(View.VISIBLE);
+        holder.revealView.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * Classe wrapper degli elementi della vista
+     */
+    public static class ViewHolder {
+        public final Toolbar toolbar;
+        public final TextView toolbarTitle;
+        public final FloatingActionButton startFabButton;
+        public final View revealView;
+        public final View revealBackgroundView;
+        public final View destinationView;
+        public final View originView;
+        public final TextView destinationViewText;
+        public final TextView originViewText;
+        public final TextView destinationViewPlaceholder;
+        public final TextView originViewPlaceholder;
+
+
+        public ViewHolder(View view) {
+            toolbar = (Toolbar) view.findViewById(R.id.navigation_toolbar);
+            toolbarTitle = (TextView) view.findViewById(R.id.navigation_toolbar_textview_title);
+            startFabButton = (FloatingActionButton) view.findViewById(R.id.navigation_fab_start);
+            revealView = view.findViewById(R.id.reveal_view);
+            revealBackgroundView = view.findViewById(R.id.reveal_background_view);
+
+            destinationView = view.findViewById(R.id.navigation_input_destination);
+            destinationViewText = (TextView) destinationView.findViewById(R.id.text);
+            destinationViewPlaceholder = (TextView) destinationView.findViewById(R.id.placeholder);
+            originView = view.findViewById(R.id.navigation_input_origin);
+            originViewText = (TextView) originView.findViewById(R.id.text);
+            originViewPlaceholder = (TextView) originView.findViewById(R.id.placeholder);
+
+        }
     }
 }
