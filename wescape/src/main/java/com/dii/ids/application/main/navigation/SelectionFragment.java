ee7d42c1e28e888a46995941f932a899c3a23732
toioski <toioski@hotmail.it>
2016-02-16 10:23:55 +0100

    Aggiunte iocne ricerca e localizzazione

--- wescape/src/main/java/com/dii/ids/application/main/navigation/SelectionFragment.java
+++ wescape/src/main/java/com/dii/ids/application/main/navigation/SelectionFragment.java
@@ -1,7 +1,6 @@
 package com.dii.ids.application.main.navigation;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
