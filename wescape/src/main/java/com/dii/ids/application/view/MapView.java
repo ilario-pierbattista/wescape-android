af13107cdb217de0669e6aade91b7d459e106d61
toioski <toioski@hotmail.it>
2016-05-23 16:11:37 +0200

    inizio creazione MapView

---
+++ wescape/src/main/java/com/dii/ids/application/view/MapView.java
@@ -0,0 +1,23 @@
+package com.dii.ids.application.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class MapView extends LinearLayout {
+    public MapView(Context context) {
+        super(context);
+    }
+
+    public MapView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MapView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public MapView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
