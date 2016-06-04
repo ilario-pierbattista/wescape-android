7a7f2b6965091d8119e349f2bf23e73d4f699bc5
toioski <toioski@hotmail.it>
2016-06-04 12:03:27 +0200

    cambiati Edittext in TextInputEditText

--- wescape/src/main/java/com/dii/ids/application/main/authentication/AuthenticationActivity.java
+++ wescape/src/main/java/com/dii/ids/application/main/authentication/AuthenticationActivity.java
@@ -1,16 +1,12 @@
 package com.dii.ids.application.main.authentication;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.Window;
 
 import com.dii.ids.application.R;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
